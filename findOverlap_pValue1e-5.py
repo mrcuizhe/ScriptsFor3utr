3bfea6deceb98e96cbbcf1a6fe5a2775657770bc
Zhe Cui <mrcuizhe@gmail.com>
2018-01-29 17:58:07 -0600

    First test

---
+++ findOverlap_pValue1e-5.py
@@ -0,0 +1,6 @@
+#To find the overlap between 3utrQTL and eQTL
+#Zhe Cui -built on Jan 29 4:29 pm
+file_3utr = "/mount/weili2/Lei/GTEx_3UTR-QTLs/Adipose-Subcutaneous.cis_eqtl.txt"
+file_eQtl = "/mount/weili2/Lei/GTEx_eQTL_SNPs/GTEx_Analysis_v7_eQTL_all_associations/Adipose_Subcutaneous.allpairs.txt"
+file_overlap = "/"
+
